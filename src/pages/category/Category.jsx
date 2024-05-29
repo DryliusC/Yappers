00516c4662d686f7d7954200d71fcce230fa7713
DryliusC <dryliuschristianc@gmail.com>
2024-05-29 18:57:54 +0700

    Login page kelar

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Category.scss";
+import Slider from "infinite-react-carousel";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
-export default function cat() {
+export default function Category() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const filter = [
+    "Recommended",
+    "Marketing",
+    "Graphic Design",
+    "Websites",
+    "Writing",
+    "Business",
+    "Video & Photography",
+    "Programs",
+  ];
+
   return (
-    <div>cat</div>
-  )
+    <div className="cat">
+      <div className="container">
+        <div className="recommendation">
+          <div className="text">
+            <h3>Our Recommendations</h3>
+            <h1>FIND THE PERFECT SERVICE!</h1>
+          </div>
+          <div className="card">
+            <Slider slidesToShow={3} arrowsScroll={3} className="recSlide">
+              {cards.map((card) => (
+                <CatCard item={card} key={card.id} />
+              ))}
+            </Slider>
+          </div>
+        </div>
+        <div className="menu">
+          {filter.map((text, index) => (
+            <span
+              key={index}
+              className={`filter-item ${activeIndex === index ? "active" : ""}`}
+              onClick={() => setActiveIndex(index)}
+            >
+              {text}
+            </span>
+          ))}
+        </div>
+        <div className="showCards">
+          <Slider slidesToShow={5} arrowsScroll={5}>
+            {cards.map((card) => (
+              <CatCard item={card} key={card.id} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-import "./Login.scss"
+import React from "react";
+import "./Login.scss";
 
 export default function Login() {
   return (
-    <div>login</div>
-  )
+    <div className="login">
+      <div className="container">
+        <div className="left">
+          <h1>Hello! Welcome Back</h1>
+          <form action="">
+            <div className="formContainer">
+              <label for="email">
+                <b>Email</b>
+              </label>
+              <input
+                type="text"
+                placeholder="Example@gmail.com"
+                name="email"
+                required
+              />
+
+              <label for="psw">
+                <b>Password</b>
+              </label>
+              <input
+                type="password"
+                placeholder="at least 8 characters"
+                name="psw"
+                required
+              />
+
+              <p>Forgot Password?</p>
+              <button type="submit">Sign-in</button>
+            </div>
+          </form>
+          <div className="seperator">
+            <span className="line"></span>
+            <span>Or</span>
+            <span className="line"></span>
+          </div>
+          <p>
+            Don't have an account? <span>Sign-up</span>
+          </p>
+        </div>
+        <div className="right">
+          <img src="../../../img/left_home.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
